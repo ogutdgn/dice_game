7996c5b9748ed65182ddb05e8836970aad24a333
ogutdgn <ogutdgn@gmail.com>
2022-03-06 20:06:22 +0300

    dice_game

--- app.js
+++ app.js
@@ -10,44 +10,45 @@ const role_dice_btn = document.querySelector(".role-dice");
 const hold_btn = document.querySelector(".hold")
 const newgame_btn = document.querySelector(".btn_new")
 
+const turn_now = document.querySelector(".turn");
+const no_turn = document.querySelector(".no-turn");
 
-//function *************
+//function *****
 let first = "first";
 let second = "second";
 let turn = first;
 
 const whos_turn = () => {
     if (turn == first){
+        turn_now.className = "no-turn";
+        no_turn.className = "turn";
         turn = second;
     }
     else if (turn == second) {
+        turn_now.className = "turn";
+        no_turn.className = "no-turn";
         turn = first;
     }
     // console.log(turn);
 };
-//*************
+//*****
 
-// hold_btn.addEventListener("click", () => {
-//     whos_turn();
-//     // if (turn == first) {
-//     //     turn = second;
-//     // }
-//     // else if (turn == second) {
-//     //     turn = first;
-//     // }
-//     // console.log(turn);
-// });
 
 let total = 0;
 let total2 = 0
+
 role_dice_btn.addEventListener("click", () => {
     let dice = Math.floor((Math.random() * 6) + 1);
     // console.log(dice);
 
     if (dice == 1) {
         whos_turn();
+        
+        total = 0;
         current_fp_value.value = 0;
         current_fp_value.innerHTML = current_fp_value.value;
+
+        total2 = 0;
         current_sp_value.value = 0;
         current_sp_value.innerHTML = current_sp_value.value;
     }
@@ -72,7 +73,8 @@ role_dice_btn.addEventListener("click", () => {
 first_player_value.value = 0;
 second_player_value.value = 0;
 hold_btn.addEventListener("click", () => {
-    
+    dice_value.value = 0;
+    dice_value.innerHTML = dice_value.value;
     // current_fp_value.textContent = 0;
     if (turn == first) {
         // console.log(first_player_value.value);
@@ -93,4 +95,18 @@ hold_btn.addEventListener("click", () => {
         current_sp_value.innerHTML = current_sp_value.value;
     }
     whos_turn();
+
+    setTimeout(() => {
+        if (first_player_value.value >= 50) {
+            alert("Player 1 WON")
+            window.location.reload();
+        }else if(second_player_value.value >= 50) {
+            alert("Player 2 WON")
+            window.location.reload();
+        }
+    }, 0);
 });
+
+// newgame_btn.addEventListener("click", () => {
+//     window.location.reload();
+// });
